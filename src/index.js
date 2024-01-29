40808f8dbd24f36297a5f14ec9715b188b6cbff0
hafidz491 <hafidzhidayatulloh491@gmail.com>
2024-01-29 13:34:17 +0700

    update dark mode + fitur

--- src/index.js
+++ src/index.js
@@ -7,14 +7,52 @@ hamburger.addEventListener("click", function () {
   navMenu.classList.toggle("hidden");
 });
 
+// CLICK EVERYWHERE TO CLOSE THE MENU TOGGLE
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
 // NAVBAR FIXED
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const btnTop = document.querySelector("#to-top");
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
+    btnTop.classList.remove("hidden");
+    btnTop.classList.add("flex");
   } else {
     header.classList.remove("navbar-fixed");
+    btnTop.classList.remove("flex");
+    btnTop.classList.add("hidden");
   }
 };
+
+// DARK MODE TOGGLE
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+// MOVING TOGGLE ACCORDING THEME
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
